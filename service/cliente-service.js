87bfe553729fe5fbc171300548657d365164e4b2
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-22 22:25:52 -0300

    final commit

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,7 +1,13 @@
 
 // Utilizando fetch
 const listaClientes = () => {
-  return fetch(`http://localhost:3000/profile`).then(resposta => resposta.json())
+  return fetch(`http://localhost:3000/profile`)
+  .then(resposta => {
+    if (resposta.ok) {
+      return resposta.json()
+    }
+    throw new Error("Não foi possível listar os clientes")
+  })
 }
 
 const criaCliente = (nome, email) => {
@@ -16,19 +22,30 @@ const criaCliente = (nome, email) => {
     })
   })
   .then(resposta => {
-    return resposta.body
+    if (resposta.ok) {
+      return resposta.body
+    }
+    throw new Error("Não foi possível criar um cliente")
   })
 }
 
 const removeCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`, {
     method: 'DELETE',
+  }).then(resposta => {
+    if(!resposta.ok) {
+      throw new Error ("Não foi possível remover um cliente")
+    }
   })
 }
 
 const detalhaCliente = (id) => {
-  return fetch(`http://localhost:3000/profile/${id}`).then(resposta => {
-    return resposta.json()
+  return fetch(`http://localhost:3000/profile/${id}`)
+  .then(resposta => {
+    if (resposta.ok) {
+      return resposta.body
+    }
+    throw new Error("Não foi possível detalhar o cliente")
   })
 }
 
@@ -44,7 +61,10 @@ const atualizaCliente = (id, nome, email) => {
     })
   })
   .then(resposta => {
-    return resposta.json()
+    if(resposta.ok) {
+      return resposta.json()
+    }
+    throw new Error("Não foi possível atualizar o cliente")
   })
 }
 
